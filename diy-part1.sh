ee695b0751013072f3444c53f6797d6320567c63
cloudysky9 <64485673+cloudysky9@users.noreply.github.com>
2021-01-22 22:55:58 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -23,3 +23,5 @@ git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-v
 git clone https://github.com/cloudysky9/luci-theme-rosy.git package/lean/luci-theme-rosy
 git clone https://github.com/xiaorouji/openwrt-passwall.git package/lean/openwrt-passwall
 #git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/lean/luci-app-jd-dailybonus
+git clone https://github.com/vernesong/OpenClash.git package/lean/OpenClash
+git clone https://github.com/frainzy1477/luci-app-clash.git package/leanluci-app-clash
