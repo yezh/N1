2e2446a3d7d4fc69399473da41f1444f952921a6
cloudysky9 <64485673+cloudysky9@users.noreply.github.com>
2021-02-13 21:17:36 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -27,4 +27,3 @@ rm -rf package/lean/luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config.git package/lean/luci-app-argon-config
 git clone https://github.com/lisaac/luci-app-dockerman.git package/lean/luci-app-dockerman
-#git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/lean/luci-app-jd-dailybonus
