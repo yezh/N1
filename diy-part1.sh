da0a90794c5e15733fe32e2a42fc9b0ca770cae4
myvmiao <76433043+myvmiao@users.noreply.github.com>
2021-04-23 04:39:33 +0000

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,7 +17,7 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 #sed -i '$a src-git kenzok https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-#sed -i '$a src-git ddnsto https://github.com/linkease/nas-packages' feeds.conf.default
+sed -i '$a src-git ddnsto https://github.com/linkease/nas-packages' feeds.conf.default
 # 扩充插件包
 git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
 git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
