3465d392ee713e1ed6fad4b6f0047d8f726ff911
Ye, Zhenhua <i@yezh.net>
2021-06-09 20:28:33 +0800

    修正配置

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,10 +14,14 @@
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # 添加feed源 kenzok8是op的常用软件包及passwall基本依赖
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-#sed -i '$a src-git kenzok https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-#sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-#sed -i '$a src-git node https://github.com/nxhack/openwrt-node-packages' feeds.conf.default
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git kenzok https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git node https://github.com/nxhack/openwrt-node-packages' feeds.conf.default
+sed -i '$a src-git weifuture https://github.com/hongweifuture/openwrt-packages' feeds.conf.default
+sed -i '$a src-git pwdep https://github.com/hongweifuture/pwdep.git' feeds.conf.default
+sed -i '$a src-git luci https://github.com/openwrt/luci.git' feeds.conf.default
+
 
 # 扩充插件包
 #svn co https://github.com/Lienol/tcping package/tcping package/tcping
@@ -31,9 +35,9 @@ git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luc
 git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
 #git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
 git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
+git clone https://github.com/brvphoenix/wrtbwmon.git package/wrtbwmon
 git clone https://github.com/brvphoenix/luci-app-wrtbwmon.git package/luci-app-wrtbwmon
 git clone https://github.com/nodejs/node.git package/node
-git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
-git clone https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
+git clone https://github.com/stonegr/luci-app-samba.git package/luci-app-samba
 #./scripts/feeds update -a
 #./scripts/feeds install -a
